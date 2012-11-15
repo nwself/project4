a71271f06f7ac5ded8826b09ccaaa195771929aa
Nathan Self <nwself@cs.vt.edu>
2012-11-15 18:04:50 -0500

    Added RSS for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,7 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @projects }
+      format.rss { render layout: false }
     end
   end
 
