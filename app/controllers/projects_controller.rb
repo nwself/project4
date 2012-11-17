e10e93a0d981c5725e23e530c327b8d1d159f069
Nathan Self <nwself@cs.vt.edu>
2012-11-17 01:11:30 -0500

    Allow feed for everyone (so RSS can find it)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, except: :feed
 
   def feed
     @projects = current_user.projects
