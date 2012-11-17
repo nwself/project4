57730a55752dd41d6badeef9fda6b235a14748db
Nathan Self <nwself@cs.vt.edu>
2012-11-16 22:12:15 -0500

    Add username to header, fix up user_proj_rel

--- app/models/user_project_rel.rb
+++ app/models/user_project_rel.rb
@@ -1,10 +1,12 @@
 class UserProjectRel < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
   has_many :task_user_proj_rels,:dependent => :destroy
   has_many :tasks, :through => :task_user_proj_rels
   has_many :bugs, :through => :task_user_proj_rels
-    attr_accessible :role, :user_id, :project_id, 
+
+  attr_accessible :role, :user_id, :project_id, inclusion: { :in => %w(Administrator Developer Tester Client) }
     
     def username()
       User.find(user_id).username
