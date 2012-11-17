d4616553f52a7e2f0f0435391ea32a4070a519e4
Krunal Doshi <doshikrunals@gmail.com>
2012-11-17 12:06:52 -0500

    All links rendered on basis of roles
    Project,task,bugs,resources and to-dolist links are rendered on the
    basis of logged in user role.
    Broadly speaking, adding and deleting is restricted to Adminsitrators,
    while all users can edit and view the Thingies :)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,7 +24,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1.json
   def show
     @project = Project.find(params[:id])
-
+    @isAdmin = @project.user_project_rels.find_by_user_id(current_user).role== "Administrator" 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @project }
